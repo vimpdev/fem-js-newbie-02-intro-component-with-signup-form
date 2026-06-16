64b11127fa908b3d686f254879d067f8b07eac00
Vanessa Mendez <252469587+vimpdev@users.noreply.github.com>
2026-06-16 11:29:08 -0500

    feat: implement form validation states

---
+++ src/scripts/main.js
@@ -0,0 +1,67 @@
+const signupForm = document.getElementById("signup-form");
+const inputs = signupForm.querySelectorAll("input");
+
+function showError(input) {
+  const field = input.closest(".field");
+  const icon = field.querySelector("img");
+  const errorMessage = field.querySelector("p");
+
+  input.setAttribute("aria-invalid", "true");
+
+  errorMessage.textContent = getErrorMessage(input);
+
+  icon.hidden = false;
+  errorMessage.hidden = false;
+}
+
+function hideError(input) {
+  const field = input.closest(".field");
+  const icon = field.querySelector("img");
+  const errorMessage = field.querySelector("p");
+
+  input.removeAttribute("aria-invalid");
+
+  icon.hidden = true;
+  errorMessage.hidden = true;
+}
+
+function getErrorMessage(input) {
+  if (input.validity.valueMissing) {
+    return `${input.placeholder} cannot be empty`;
+  }
+
+  if (input.validity.typeMismatch) {
+    return "Looks like this is not an email";
+  }
+
+  return "";
+}
+
+signupForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  let isFormValid = true;
+
+  inputs.forEach((input) => {
+    if (input.checkValidity()) {
+      hideError(input);
+    } else {
+      showError(input);
+      isFormValid = false;
+    }
+  });
+
+  if (isFormValid) {
+    console.log("form enviado");
+    signupForm.reset();
+  }
+
+});
+
+inputs.forEach((input) => {
+  input.addEventListener("input", () => {
+    if (input.checkValidity()) {
+      hideError(input);
+    }
+  });
+});
